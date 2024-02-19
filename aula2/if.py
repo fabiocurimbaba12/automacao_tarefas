d633c8b9c63199ff38afe5951831886c61708ae4
fabiocurimbaba12 <fabiocurimbaba12@gmail.com>
2024-02-19 08:37:30 -0300

    Estudo sobre estrutura de condição

---
+++ aula2/if.py
@@ -0,0 +1,12 @@
+print('Digite sua idade: ')
+idade = int(input())
+
+if idade < 12:
+    print('Você é uma criança')    
+elif idade < 18:
+    print('Você é um adolescente')
+elif idade < 65:
+    print('Você é um adulto')
+else:
+    print('Você é um idoso')
+
